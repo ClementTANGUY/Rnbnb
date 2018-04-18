822896f7a6e6fcf4b741914d9ced66527a8a43dd
Icardi38 <vm26190@hotmail.fr>
2018-04-18 14:29:30 +0200

    2 génis parmi les hommes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,18 +11,18 @@ class ReservationsController < ApplicationController
     room = Room.find(params[:room_id])
     today = Date.today
     reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
-         
+
     render json: reservations
   end
-    
+
   def preview
     start_date = Date.parse(params[:start_date])
     end_date = Date.parse(params[:end_date])
-        
+
     output = {
         conflict: is_conflict(start_date, end_date)
     }
-        
+
     render json: output
   end
 
@@ -34,7 +34,7 @@ class ReservationsController < ApplicationController
 
     def is_conflict(start_date, end_date)
     room = Room.find(params[:room_id])
- 
+
     check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
     check.size > 0? true : false
 end
